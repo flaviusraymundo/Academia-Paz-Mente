404370ac4c9e17543e3fd9e99b3be906eea2e5cd
flaviusraymundo <141464857+flaviusraymundo@users.noreply.github.com>
2025-11-11 18:58:27 -0300

    Fix sanitize import extension

--- src/server/routes/admin.ts
+++ src/server/routes/admin.ts
@@ -6,7 +6,7 @@ import { isUuid, paramUuid } from "../utils/ids.js";
 import { requireAuth } from "../middleware/auth.js";
 import { requireAdmin } from "../middleware/admin.js";
 import { issueCertificate } from "../lib/certificates.js";
-import { sanitizePayloadRef, resolveMode } from "../lib/sanitize";
+import { sanitizePayloadRef, resolveMode } from "../lib/sanitize.js";
 
 const router = Router();
 
