0b88253cce633460bf7e80ee53e40d406df9a3b3
Flavius Raymundo <141464857+flaviusraymundo@users.noreply.github.com>
2025-11-16 10:32:09 -0300

    Fix API function import for Netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,6 +1,6 @@
 // netlify/functions/api.ts
 import serverless from "serverless-http";
-import app from "../../src/server/app";
+import app from "../../src/server/app.js";
 
 // inclui PNG e PDF como tipos binários
 export const handler = serverless(app, {
